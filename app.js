14686a27bad71b200153731a1b650cff5b324eaf
21julesjunior <75339052+21julesjunior@users.noreply.github.com>
2020-12-02 19:36:12 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+for (let i = 0; i <= 12; i++) {
+    for (let j = 0; j <= 12; j++) {
+        if (j == 12) {
+            console.log(i, '*', j, '=', i * j)
+            console.log('-----------');
+        } else {
+            console.log(i, '*', j, '=', i * j);
+        }
+
+    }
+}
